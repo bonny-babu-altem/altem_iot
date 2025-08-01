7726b15b72a81d58a4d9351a3046cc7e20d027d1
bonny-babu-altem <bonny.babu@altem.com>
2025-08-01 14:39:18 +0800

    moved to async programming

--- ros_workspace/src/stratasys_f170/stratasys_f170/mqtt_publisher.py
+++ ros_workspace/src/stratasys_f170/stratasys_f170/mqtt_publisher.py
@@ -1,7 +1,11 @@
+import asyncio
 import json
-from typing import Any
+from typing import Any, Optional
 import paho.mqtt.client as mqtt
+import rclpy
 from rclpy.impl.rcutils_logger import RcutilsLogger
+
+from gmqtt import Client
 
 
 class MQTT:
@@ -36,9 +40,9 @@ class MQTT:
 
     def connect(self) -> None:
         try:
+            self.__log.info('Trying to connect to ThingsBoard...')
             self.__client.connect(self.__host, self.__port, keepalive=60)
             self.__client.loop_start()
-            self.__log.info('Trying to connect to ThingsBoard...')
         except Exception as e:
             self.__log.error(f'Failed to connect: {e}')
 
@@ -72,3 +76,111 @@ class MQTT:
                 client.reconnect()
             except Exception as e:
                 self.__log.error(f'Auto-reconnect failed: {e}')
+
+
+class MQTTClient:
+    __slots__: tuple[str, ...] = (
+        '_token',
+        '_client',
+        '_topic',
+        '_host',
+        '_port',
+        '_log',
+        '_connected',
+    )
+
+    def __init__(self, token: str, log: RcutilsLogger, host: str, port: int = 1883) -> None:
+        self._token: str = token
+        self._host: str = host
+        self._port: int = port
+        self._log: RcutilsLogger = log
+
+        self._client: Client
+
+        self._topic: str = "v1/devices/me/telemetry"
+
+        self._connected = asyncio.Event()
+
+    async def _is_client(self) -> bool:
+        return hasattr(self, '_client')
+
+    async def connect(self) -> None:
+        if not await self._is_client():
+            self._log.info(f'Trying to connect to {self._host}:{self._port}')
+            self._client = Client(client_id=f'id-client-{id(self)}')
+            self._client.set_auth_credentials(self._token, None)
+
+            self._client.set_config(
+                {
+                    'reconnect_retries': 10,
+                    'reconnect_delay': 60
+                }
+            )
+
+            self._client.on_connect = self._on_connect
+            self._client.on_disconnect = self._on_disconnect
+
+            await self._client.connect(
+                self._host, self._port,
+                keepalive=60
+            )
+
+            await self._connected.wait()
+
+    async def disconnect(self) -> None:
+        if await self._is_client():
+            await self._client.disconnect()
+
+    def _on_connect(self, *_) -> None:
+        self._connected.set()
+        self._log.info('Successfully connected to ThingsBoard.')
+
+    def _on_disconnect(self, *_) -> None:
+        self._connected.clear()
+        self._log.warn("Disconnected from ThingsBoard. Reconnecting...")
+
+    async def publish_data(self, data: dict[str, Any], qos: int = 1, retain: bool = False) -> None:
+        msg: str = json.dumps(data)
+
+        if not self._connected.is_set():
+            raise RuntimeError("Client not initialized. Use 'connect'.")
+
+        self._log.info(
+            f'Publishing data [topic: {self._topic}, {self._host}:{self._port}]')
+
+        self._client.publish(self._topic, msg, qos, retain)
+
+
+async def wait_for_broker(
+        host: str, port: int = 1883,
+        delay: float = 2.0, max_attempts: int = 0,
+        logger: Optional[RcutilsLogger] = None) -> bool:
+
+    attempt = 0
+    while rclpy.ok():
+        try:
+            _, writer = await asyncio.open_connection(host, port)
+            writer.close()
+            await writer.wait_closed()
+
+            if logger:
+                logger.info(f"Broker at {host}:{port} is reachable.")
+            return True
+
+        except (OSError, ConnectionRefusedError):
+            attempt += 1
+            msg: str = f"Broker at {host}:{port} not reachable. Attempt {attempt}"
+            if logger:
+                logger.warn(msg)
+            else:
+                print(msg)
+
+            if max_attempts and attempt >= max_attempts:
+                if logger:
+                    logger.error(
+                        f"Broker not reachable after {max_attempts} attempts.")
+                return False
+
+        await asyncio.sleep(delay)
+
+    return False
--- ros_workspace/src/stratasys_f170/stratasys_f170/printer.py
+++ ros_workspace/src/stratasys_f170/stratasys_f170/printer.py
@@ -1,14 +1,19 @@
+import asyncio
 import json
+from threading import Thread
 from typing import Any
+
+from httpx import Response
+
 import rclpy
 from rclpy.impl.rcutils_logger import RcutilsLogger
 from rclpy.node import Node
 from rclpy.publisher import Publisher
-from rclpy.timer import Timer
 from std_msgs.msg import String
-from stratasys_f170.device import get_device_status
+
+from stratasys_f170.device import DeviceRequest, wait_for_device_ip
 from stratasys_f170.xml_parser import parse_data
-from stratasys_f170.mqtt_publisher import MQTT
+from stratasys_f170.mqtt_publisher import MQTTClient, wait_for_broker
 
 
 class F170(Node):
@@ -30,37 +35,64 @@ class F170(Node):
         self.publisher_: Publisher = self.create_publisher(
             String, 'status', 10)
 
-        self.iot = MQTT(
-            token=self.access_token,
-            log=self.get_logger(),
-            host=self.tb_host,
-            port=self.tb_port
-        )
-
         self.get_logger().info(f'TB: {self.tb_host}:{self.tb_port}')
         self.get_logger().info(f'F170 IP: {self.device_ip}')
 
-        self.iot.connect()
+        _log: RcutilsLogger = self.get_logger()
 
-        self.timer: Timer = self.create_timer(0.005, self.publish_callback)
+        self.request = DeviceRequest(url=self.device_ip, log=_log)
+        self.iot = MQTTClient(
+            token=self.access_token, log=_log,
+            host=self.tb_host, port=self.tb_port
+        )
 
-    def publish_callback(self) -> None:
-        parsed_data: None | dict[str, Any] = self.get_device_data()
-        if parsed_data is None:
-            self.get_logger().warning('No data received from f170 device')
-            return
+        self.loop: asyncio.AbstractEventLoop = asyncio.new_event_loop()
+        self.thread = Thread(target=self.start_loop, daemon=True)
+        self.thread.start()
+        asyncio.run_coroutine_threadsafe(self.publish_data(), self.loop)
 
-        self.get_logger().debug(f'Received {parsed_data}')
+    def start_loop(self) -> None:
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
 
-        self.msg = String()
-        self.msg.data = json.dumps(parsed_data)
+    async def publish_data(self) -> None:
+        _log: RcutilsLogger = self.get_logger()
 
-        self.iot.publish_data(data=parsed_data)
-        self.publisher_.publish(self.msg)
+        await wait_for_device_ip(ip=self.device_ip, delay=5, logger=_log)
+        await wait_for_broker(host=self.tb_host, delay=5, logger=_log)
 
-    def get_device_data(self) -> None | dict[str, Any]:
-        xml_data: str = get_device_status(ip=self.device_ip)
-        return parse_data(xml_data=xml_data)
+        await self.request.connect()
+        await self.iot.connect()
+
+        try:
+            while rclpy.ok():
+                try:
+                    response: Response = await self.request.get()
+
+                except Exception as e:
+                    self.get_logger().error(f"HTTP fetch failed: {e}")
+                    # await wait_for_device_ip(ip=self.device_ip, delay=5, logger=self.get_logger())
+                    continue
+
+                parsed_data: None | dict[str, Any] = parse_data(
+                    xml_data=response.text)
+                if parsed_data is None:
+                    self.get_logger().warning('No data received from f170 device')
+                    return
+
+                self.msg = String()
+                self.msg.data = json.dumps(parsed_data)
+
+                await self.iot.publish_data(data=parsed_data)
+                self.publisher_.publish(self.msg)
+        finally:
+            await self.request.disconnect()
+
+    def destroy_node(self) -> None:
+        self.loop.call_soon_threadsafe(self.loop.stop)
+        self.thread.join()
+
+        super().destroy_node()
 
 
 def main(args=None):
